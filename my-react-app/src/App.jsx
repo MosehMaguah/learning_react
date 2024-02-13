a046f39ae06ea694496bda55b958dcd136eedff1
mosehmaguah <mosehmaguah@gmail.com>
2024-02-14 01:54:26 +0300

    applying css styling to components

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from "./Header.jsx";
+import Home from "./Home.jsx";
+import Footer from "./Footer.jsx";
+import Card from "./Card.jsx";
+import Student from "./Student.jsx";
+import UserGreeting from "./UserGreeting.jsx";
+import List from "./List.jsx";
+import Button from './Button.jsx'
+import ProfilePic from "./ProfilePic.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const fruits = [
+    { id: 1, name: "banana", calories: 95 },
+    { id: 2, name: "orange", calories: 100 },
+    { id: 3, name: "watermelon", calories: 30 },
+    { id: 4, name: "apple", calories: 200 },
+    { id: 5, name: "pineapple", calories: 170 },
+  ];
+
+  const vegetables = [
+    { id: 6, name: "potatoes", calories: 195 },
+    { id: 7, name: "celery", calories: 10 },
+    { id: 8, name: "carrots", calories: 304 },
+    { id: 9, name: "corn", calories: 206 },
+    { id: 0, name: "broccoli", calories: 17 },
+  ];
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <UserGreeting isLoggedIn={true} />
+
+      <Button />
+      <ProfilePic/>
+
+      {fruits.length > 0 && <List items={fruits} category="Fruits" />}
+      {vegetables.length > 0 && (
+        <List items={vegetables} category="Vegetables" />
+      )}
+
+      <Home />
+      <Student name="Spongebob" age={30} isStudent={true} />
+      <Student name="Eugene Crabs" age={60} isStudent={false} />
+      <Student name="Patrick" age={27} isStudent={false} />
+      <Student name="Sandy Cheeks" age={25} isStudent={false} />
+      <Student />
+      <Card />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
